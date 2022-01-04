7900dee4ead824c47465532e7879f29d33bbfe26
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-04 00:06:56 +0000

    hasta ejercicio 4

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,14 @@
-var age = prompt('What is your age?');
+var age = parseInt (  prompt('What is your age?')  );
+
+
+
+
 
 // Your code below:
+//let result = parseInt(age) + 10;
+
+
+
+console.log(age+10);
+
+
