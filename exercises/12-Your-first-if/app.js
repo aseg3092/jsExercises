1ce516ef4ac8412dc8dd528cd72a7125bdc96eb4
aseg3092 <gabrielsalazar3092@gmail.com>
2022-01-04 00:43:54 +0000

    hasta ejercicio 13

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,36 @@
-var total = prompt('How many km are left to go?');
+var total =  parseInt(prompt('How many km are left to go?'))  ;
+
 
 // Your code below:
+
+// if(total<=50) 
+// {
+//     console.log("I'm parking. I'll see you right now");
+// }
+
+
+// if(total>50 && total <=100) 
+// {
+//     console.log("We'll be there in 5 minutes");
+// }
+
+// if(total >100) 
+// {
+//     console.log("We still have a bit of driving left to go");
+// }
+
+
+if(total<=50 )
+{
+    console.log(total);
+    console.log("I'm parking. I'll see you right now");
+}
+else if (total>50 && total<=100)
+{
+    console.log("We'll be there in 5 minutes");
+}
+else
+{
+    console.log(total);
+    console.log("We still have a bit of driving left to go");
+}
